da05c18331dbfb978adbf85d4c4db64bcb8a1f91
Terrii <minimatt10@gmail.com>
2019-08-20 12:38:51 +0930

    Character Viewer, small updates

--- src/components/pages/inspect/GenerateUserCharacterView.js
+++ src/components/pages/inspect/GenerateUserCharacterView.js
@@ -3,12 +3,14 @@ import Error from '../../modules/Error';
 import * as Misc from '../../Misc';
 
 export function generate(profileInfo) {
+  const CharactersInfo = profileInfo.characters.data;
+
   return (
     <div className="inspectBox" id="InspectBoxCharacterViewer">
       <div className="inspectBoxTitle"> Character Viewer </div>
       <div className="inspectBoxContent">
         <div className="inspectBoxCharacterSelect">
-          
+          { Object.keys(CharactersInfo).map(function(key, value) { return ( GetCharacter(CharactersInfo[key]) ); }) }
         </div>
         <div className="inspectBoxCharacterView">
           {
@@ -17,5 +19,12 @@ export function generate(profileInfo) {
         </div>
       </div>
     </div>
+  );
+}
+
+function GetCharacter(characterData) {
+  //This is where the stuff happens
+  return (
+    <div>{ characterData.light }</div>
   );
 }
