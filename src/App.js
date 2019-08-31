dc35a975746325dcd4efa3d46139d98c8f3bab2d
Terrii <minimatt10@gmail.com>
2019-08-31 12:05:16 +0930

    Removed "Link" un-used requirement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //Required Libraries
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //Modules
 import Header from './components/modules/Header';
