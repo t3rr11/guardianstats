64a24750cec10fc4542e54f80144544611e19236
Terrii <minimatt10@gmail.com>
2019-09-01 12:03:17 +0930

    Page not found

--- src/components/pages/others/PageNotFound.js
+++ src/components/pages/others/PageNotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function PageNotFound() {
   return(
-    <React.Fragment>
-      <h1>Page Not Found</h1>
-    </React.Fragment>
+    <div className="error-container">
+      <p style={{ fontSize: "larger" }}>Uhhh... Page Not Found</p>
+      <div className="error-icon" style={{ width: "500px", backgroundImage: 'url("./images/pagenotfound.jpg")' }}></div>
+      <div style={{ marginTop: "15px" }}><Link to="/" style={{ marginTop: "40px", color: "white", fontSize: "larger" }}>Go back home...</Link></div>
+    </div>
   )
 }
 
