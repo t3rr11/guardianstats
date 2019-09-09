22566bded7975ec0ae9ffd3c21e9c9c23cf5ec16
Terrii <minimatt10@gmail.com>
2019-09-09 22:01:49 +0930

    Added changelog, css changes, added more information to exotics page.

--- src/components/pages/items/Items.js
+++ src/components/pages/items/Items.js
@@ -26,6 +26,22 @@ export class Items extends Component {
     }
   }
 
+  getItemInfo(hash, request) {
+    const { data } = this.state;
+    if(request === "name") {
+      var name = data.ManifestCollectibles[hash].displayProperties.name;
+      if(name !== "") { return <div className="title">{ name }</div> } else { return null; }
+    }
+    else if(request === "description") {
+      var description = data.ManifestCollectibles[hash].displayProperties.description;
+      if(description !== "") { return <div className="description">{ description }</div> } else { return null; }
+    }
+    else if(request === "requirements") {
+      var requirements = data.ManifestCollectibles[hash].stateInfo.requirements.entitlementUnavailableMessage;
+      if(requirements !== "") { return <div className="requirements">{ requirements }</div> } else { return null; }
+    }
+  }
+
   render() {
     //Define Consts and Variables
     const { status, statusText } = this.state.status;
@@ -48,7 +64,12 @@ export class Items extends Component {
                         data.PresentationNodes[node.presentationNodeHash].children.collectibles.map(collectible => (
                           <div key={ collectible.collectibleHash } id={ collectible.collectibleHash } className="collectibleItemContainer">
                             <img alt='Icon' src={ 'https://bungie.net' + data.ManifestCollectibles[collectible.collectibleHash].displayProperties.icon } className={ data.ManifestCollectibles[collectible.collectibleHash].obtained ? 'collectibleItemImage' : 'collectibleItemImage notAcquired' } />
-                            <div className="collectibleItemInfo">{ data.ManifestCollectibles[collectible.collectibleHash].displayProperties.name }</div>
+                            <div className="collectibleItemInfo">
+                              { console.log(data.ManifestCollectibles[collectible.collectibleHash]) }
+                              { this.getItemInfo(collectible.collectibleHash, "name") }
+                              { this.getItemInfo(collectible.collectibleHash, "description") }
+                              { this.getItemInfo(collectible.collectibleHash, "requirements") }
+                            </div>
                           </div>
                         ))
                       }
