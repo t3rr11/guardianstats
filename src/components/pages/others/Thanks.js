e81f86a9fd595a420a1445d7596eb909a1ef11bb
Terrii <minimatt10@gmail.com>
2019-09-23 20:06:52 +0930

    Edited thanks page

--- src/components/pages/others/Thanks.js
+++ src/components/pages/others/Thanks.js
@@ -8,6 +8,7 @@ function Thanks() {
         <h1 className="home-title">Thanks!</h1>
         <p className="home-text">Seriously, Thank you so damn much for the donation. You really help me keep this project alive and i am so very grateful to you for that! Keep slaying Guardian!</p>
         <img src="/images/thanks.gif" width="500px" height="375px" />
+        <p className="home-text" style={{ marginTop: "10px" }}>If its more than $5 message me on discord (Terrii#5799) and i'll get you added to the donar list. </p>
       </div>
     </div>
   )
